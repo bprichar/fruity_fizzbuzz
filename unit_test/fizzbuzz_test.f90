fa97375436ecf28ec7d8eb0df9fde9500eef0a55
Brad Richardson <brichardson@structint.com>
2017-10-16 21:39:15 -0700

    rename test routines

--- unit_test/fizzbuzz_test.f90
+++ unit_test/fizzbuzz_test.f90
@@ -7,25 +7,25 @@ implicit none
 
 contains
 
-subroutine test_fizzbuzz_for_normal_numbers_should_return_them_as_string
+subroutine test_fizzbuzz_for_normal_numbers_are_number_as_string
 
 call assertEquals("1", fizzbuzz(1))
 call assertEquals("2", fizzbuzz(2))
 
-end subroutine test_fizzbuzz_for_normal_numbers_should_return_them_as_string
+end subroutine test_fizzbuzz_for_normal_numbers_are_number_as_string
 
-subroutine test_fizzbuzz_for_divisible_by_three_should_return_fizz
+subroutine test_fizzbuzz_for_divisible_by_three_are_fizz
 
 call assertEquals("fizz", fizzbuzz(3))
 call assertEquals("fizz", fizzbuzz(6))
 
-end subroutine test_fizzbuzz_for_divisible_by_three_should_return_fizz
+end subroutine test_fizzbuzz_for_divisible_by_three_are_fizz
 
-subroutine test_fizzbuzz_for_divisible_by_five_should_return_buzz
+subroutine test_fizzbuzz_for_divisible_by_five_are_buzz
 
 call assertEquals("buzz", fizzbuzz(5))
 call assertEquals("buzz", fizzbuzz(10))
 
-end subroutine test_fizzbuzz_for_divisible_by_five_should_return_buzz
+end subroutine test_fizzbuzz_for_divisible_by_five_are_buzz
 
 end module fizzbuzz_test
