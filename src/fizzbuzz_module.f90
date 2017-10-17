083c0885dfe386a989a9c58346a3eb4f06dab19c
Brad Richardson <brichardson@structint.com>
2017-10-16 21:28:21 -0700

    Handle regular numbers

--- src/fizzbuzz_module.f90
+++ src/fizzbuzz_module.f90
@@ -7,8 +7,9 @@ contains
 function fizzbuzz(number) result(result)
 character(len=:), allocatable :: result
 integer, intent(in) :: number
-if (number == 0) continue
-result = ""
+character(len=32) :: temp
+write(temp, '(I0)') number
+result = trim(temp)
 end function fizzbuzz
 
 end module fizzbuzz_module
--- unit_test/fizzbuzz_test.f90
+++ unit_test/fizzbuzz_test.f90
@@ -7,4 +7,11 @@ implicit none
 
 contains
 
+subroutine test_fizzbuzz_for_normal_numbers_should_return_them_as_string
+
+call assertEquals("1", fizzbuzz(1))
+call assertEquals("2", fizzbuzz(2))
+
+end subroutine test_fizzbuzz_for_normal_numbers_should_return_them_as_string
+
 end module fizzbuzz_test
